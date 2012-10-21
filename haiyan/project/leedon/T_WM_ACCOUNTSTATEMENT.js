cabce13e0c513b2efda0c30206708ffe2e65ffcc
wwwcomy <wwwcomy@987b8463-eb34-b6a0-9039-18afa5b108b0>
2012-10-21 14:51:12 +0000

    对账单JS

---
+++ haiyan/project/leedon/T_WM_ACCOUNTSTATEMENT.js
@@ -0,0 +1,36 @@
+﻿
+function renderCheckboxSelectionModel(v, meta, record, rowIndex, i, store) {
+	if (store.treeField && record.get('CODE').length<6)
+		return '';
+    return '<div class="x-grid3-row-checker">&#160;</div>';
+}
+
+Ext.onReady(function(){
+	try {
+		if ($('ID')!=null && $('ID').value=='-1')
+			alert('ID lost.Not support!)');
+		
+		if ($('BILL_STATUS'))
+			if ($('BILL_STATUS').value=='init'||Hy.isEmpty($('BILL_STATUS').value)) {
+			} else {
+			}
+	}catch(E){}
+	var g;
+	g=Ext.getCmp('SUBGRID');
+	
+	var resizer = new Ext.Resizable('SUBGRID', {
+		handles: 'e' 
+		,resizeChild:true
+		,pinned: true
+	});
+	resizer.on('resize', function() {
+		var box = resizer.getEl().getSize();
+		this.setSize(box);
+		//this.syncSize();
+		this.doLayout();
+	}, g);
+	$("TITLE_L").innerHTML="<div style='text-align:center;font-size:40px;margin:10px 0 10px 0'>&nbsp;供应商月度对账单</div>";
+	$("TITLE").parentNode.parentNode.remove()
+	$("TITLE_L").setAttribute("colspan",'4')
+	$("CONTENT").parentNode.parentNode.remove()
+});

25d4e3949147a47a51ba675654e25c948c014b7d
wwwcomy <wwwcomy@987b8463-eb34-b6a0-9039-18afa5b108b0>
2012-10-23 17:23:23 +0000

    字段显示+JS处理

--- trunk/haiyan/project/leedon/T_WM_ACCOUNTSTATEMENT.js
+++ trunk/haiyan/project/leedon/T_WM_ACCOUNTSTATEMENT.js
@@ -1,10 +1,4 @@
 ﻿
-function renderCheckboxSelectionModel(v, meta, record, rowIndex, i, store) {
-	if (store.treeField && record.get('CODE').length<6)
-		return '';
-    return '<div class="x-grid3-row-checker">&#160;</div>';
-}
-
 Ext.onReady(function(){
 	try {
 		if ($('ID')!=null && $('ID').value=='-1')
@@ -29,9 +23,41 @@ Ext.onReady(function(){
 		//this.syncSize();
 		this.doLayout();
 	}, g);
+	var month = $("MONTH").value,re=new RegExp("20(\\d\\d)01"),r;
+	var lastMonth;
+	r=month.match(re);
+	if(r){
+		lastMonth = "20"+(r[1]-1)+"12";
+	}else{
+		lastMonth = month-1;
+	}
 	$("TITLE_L").innerHTML="<div style='text-align:center;font-size:40px;margin:20px 0 20px 0'>&nbsp;供应商月度对账单</div>";
-	$("TITLE").parentNode.parentNode.remove()
-	$("TITLE_L").setAttribute("colspan",'4')
-	$("CONTENT").parentNode.parentNode.remove()
-	$("CONTENT_L").innerHTML="我们未能收到贵司上月发票，请您先核对上月发票再开具本月，疑问请致电(xxx)";
+	$("TITLE").parentNode.parentNode.remove();
+	$("TITLE_L").setAttribute("colspan",'4');
+	$("MONTH_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>月度:"+$("MONTH").value+"</div>";
+	$("MONTH").parentNode.parentNode.remove();
+	$("SUPPLIER_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>供应商:"+$("__SUPPLIER__NAME").value+"</div>";
+	$("__SUPPLIER__NAME").parentNode.parentNode.remove();
+	
+	$("BEGIN_DATE_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>日期:"+lastMonth+"21"+" 至 "+month+"20</div>";
+	$("BEGIN_DATE").parentNode.parentNode.remove();
+	
+
+	$("CONTENT").parentNode.parentNode.remove();
+//	$("CONTENT_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>我们未能收到贵司上月发票，请您先核对上月发票再开具本月，疑问请致电(xxx)</div>";
+	var d = new Date();
+	var year = d.getFullYear();
+	var month1 = add_zero(d.getMonth()+1);
+	var day = add_zero(d.getDate());
+	if(""+year+month1+day<month+"20"){
+		$("BILL_AMOUNT").parentNode.parentNode.remove();
+		$("BILL_AMOUNT_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>待月结帐期到达后，系统将会显示开票金额</div>";
+	}
+		
 });
+
+function add_zero(temp)
+{
+ if(temp<10) return "0"+temp;
+ else return temp;
+}

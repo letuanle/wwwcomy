7435dd198a2f16e948962f3305e2476de27c979f
wwwcomy <wwwcomy@987b8463-eb34-b6a0-9039-18afa5b108b0>
2012-11-20 01:08:12 +0000

    添加对账无发票提示

--- trunk/haiyan/project/leedon/T_WM_ACCOUNTSTATEMENT.js
+++ trunk/haiyan/project/leedon/T_WM_ACCOUNTSTATEMENT.js
@@ -23,6 +23,8 @@ Ext.onReady(function(){
 					Hy.setValue('LAST_R_AMOUNT', d.payed); //上月实付
 					var topay = parseInt(d.topay)+parseInt(d.all)-parseInt(d.payed);
 					Hy.setValue('ALL_AMOUNT', topay); //本月应付
+					if(!d.getbill)
+						$("CONTENT_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px;color:red'>我们未能收到贵司上月发票，请您先核对<br>上月发票再开具本月，疑问请致电本公司</div>";
 				}
 			}
 		});	
@@ -34,16 +36,14 @@ Ext.onReady(function(){
 	$("TITLE_L").setAttribute("colspan",'4');
 	$("MONTH_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>月度:"+$("MONTH").value+"</div>";
 	$("MONTH").style.display="none"
-//	$("MONTH").parentNode.parentNode.remove();
+
 	$("SUPPLIER_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>供应商:"+$("__SUPPLIER__NAME").value+"</div>";
 	$("__SUPPLIER__NAME").parentNode.parentNode.remove();
 	
 	$("BEGIN_DATE_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>日期:"+lastMonth+"21"+" 至 "+month+"20</div>";
 	$("BEGIN_DATE").parentNode.parentNode.remove();
-	
 
 	$("CONTENT").parentNode.parentNode.remove();
-//	$("CONTENT_L").innerHTML="<div style='font-size:10px;margin:0px 0px 0px 15px'>我们未能收到贵司上月发票，请您先核对上月发票再开具本月，疑问请致电(xxx)</div>";
 	var d = new Date();
 	var year = d.getFullYear();
 	var month1 = add_zero(d.getMonth()+1);

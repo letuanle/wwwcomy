12deba602e00e39512f32b1db9201ab3575daba0
wwwcomy <wwwcomy@987b8463-eb34-b6a0-9039-18afa5b108b0>
2012-12-13 06:20:30 +0000

    Quartz状态代码修改提交

--- trunk/test/quartz/QuartzParaTest.java
+++ trunk/test/quartz/QuartzParaTest.java
@@ -18,15 +18,22 @@ public class QuartzParaTest {
 		Scheduler scheduler = factory.getScheduler();
 		Date runTime = DateBuilder.nextGivenSecondDate(null, 10);
 		JobDetail job = JobBuilder.newJob(MyParaJob.class).withIdentity("job1", "group1").build();
+		job.getJobDataMap().put(MyParaJob.JOB_DATA_MAP_KEY, 10);
 		// 每隔五秒执行，重复4次
 		Trigger trigger = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1")
 				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(5).withRepeatCount(4)).startAt(runTime).build();
 		scheduler.scheduleJob(job, trigger);
+
+		// 再来个有状态的Job
+		job = JobBuilder.newJob(MyParaJob.class).withIdentity("job2", "group1").build();
+		job.getJobDataMap().put(MyParaJob.JOB_DATA_MAP_KEY, -10);
+		trigger = TriggerBuilder.newTrigger().withIdentity("trigger2", "group1")
+				.withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(5).withRepeatCount(4)).startAt(runTime).build();
+		scheduler.scheduleJob(job, trigger);
 		scheduler.start();
 		try {
-			Thread.sleep(65L * 1000L);
+			Thread.sleep(35L * 1000L);
 		} catch (Exception e) {
-
 		}
 		scheduler.shutdown(true);
 	}

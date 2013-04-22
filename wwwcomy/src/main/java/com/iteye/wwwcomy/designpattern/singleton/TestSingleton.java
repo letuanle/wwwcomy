39abaa9955a6499052cbe1665e0792dd3587b4e1
wwwcomy <wwwcomy@987b8463-eb34-b6a0-9039-18afa5b108b0>
2013-04-22 00:55:07 +0000

    单例注释

--- wwwcomy/src/main/java/com/iteye/wwwcomy/designpattern/singleton/TestSingleton.java
+++ wwwcomy/src/main/java/com/iteye/wwwcomy/designpattern/singleton/TestSingleton.java
@@ -1,5 +1,11 @@
 package com.iteye.wwwcomy.designpattern.singleton;
 
+/**
+ * 测试单例，另外用内部类的单例见com.iteye.wwwcomy.executers.ThreadPool
+ * 
+ * @author Liuxn
+ * 
+ */
 public class TestSingleton {
 
 	private static TestSingleton instance = null;
